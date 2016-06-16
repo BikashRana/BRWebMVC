33148b1885ae6eba18a84abaafd02e41dd2e94a6
BikashRana <bikash.rana1@gmail.com>
2016-06-16 13:01:51 -0400

    First Commit

---
+++ BRwebMVC/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,15 @@
+﻿using BRwebMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BRwebMVC.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+
+        public List<Customer> Customers { get; set; }
+    }
+}
